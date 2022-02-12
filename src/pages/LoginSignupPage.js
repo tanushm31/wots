3b3f4333098ff0d2e9521803d0f054ade62ea6c9
Tanush Mahajan <tanush.mohina@gmail.com>
2022-02-12 15:08:04 +0530

    alot of changes consisting of backend crud functionality of frontend,routing enabled on pages

---
+++ src/pages/LoginSignupPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//import components
+import Navbar from '../components/Navbar'
+
+export default function LoginSignupPage() {
+  return (
+    <div>
+        <Navbar/>
+        
+    </div>
+  )
+}
